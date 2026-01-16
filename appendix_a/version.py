434a2b522410b5e89a7d98dc0f7fcf2abe9f1120
CarlosJGarcia <cgarciams@hotmail.com>
2026-01-16 11:56:06 +0100

    version.py

--- appendix_a/version.py
+++ appendix_a/version.py
@@ -1,3 +1,11 @@
+import sys
 import torch
-print("Torch version: ", torch.__version__)
-print("CUDA enabled: ", torch.cuda.is_available())
+
+print()
+print(f"Python version: {sys.version_info.major}.{sys.version_info.minor}")
+print("Torch version:", torch.__version__)
+print("Apple Silicon acceleration:", torch.backends.mps.is_available())
+print("CUDA enabled:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("CUDA Compute Platform:", torch.version.cuda)
+print()
