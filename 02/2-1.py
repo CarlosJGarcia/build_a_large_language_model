910291250f3673142f4b5b000b45fe02e99f7c66
CarlosJGarcia <cgarciams@hotmail.com>
2026-03-25 22:19:30 +0100

    Actualizacion

--- 02/2-1.py
+++ 02/2-1.py
@@ -1,4 +1,13 @@
+# Old way - manual, risky
+"""
+f = open("file.txt", "r")
+raw_text = f.read()
+f.close()  # ← easy to forget! and won't run if an error occurs above
+"""
+
+# New way, with 'Context Manager' (with ... as)
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
+
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
