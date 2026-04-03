b6a86bc82ae0650f9ac712e01a9522525942c3cd
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-03 23:52:33 +0200

    Tokenizer

--- 02/2-1.py
+++ 02/2-1.py
@@ -1,3 +1,5 @@
+import re
+
 # Old way - manual, risky
 """
 f = open("file.txt", "r")
@@ -7,7 +9,20 @@ f.close()  # ← easy to forget! and won't run if an error occurs above
 
 # New way, with 'Context Manager' (with ... as)
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    print("Reading file")
     raw_text = f.read()
 
-print("Total number of character:", len(raw_text))
+print("Total number of characters:", len(raw_text))
 print(raw_text[:99])
+print()
+
+text = "Hello, world. This, is a test."
+result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+result = [item.strip() for item in result if item.strip()]
+print(result)
+print()
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
