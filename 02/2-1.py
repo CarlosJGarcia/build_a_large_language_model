087c1d7cc0027469ed60b3fddfea1f1fc813933d
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-05 08:16:21 +0200

    Tokenizer

--- 02/2-1.py
+++ 02/2-1.py
@@ -83,7 +83,7 @@ print()
 
 
 
-# Clase Tokenizer
+# Clase Tokenizer v1
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab            #1
@@ -104,17 +104,53 @@ class SimpleTokenizerV1:
         return text 
 
 
+# Clase Tokenizer v2
+# Si una palabra del texto no existe en el Vocabulario, le asigna el token <|unk|> (que debe estar definido en el Vocabulario
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int            #1
+                        else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)    #2
+        return text
+
+
 # 4. Creo un objeto tokenizer a partir de la clase SimpleTokenizerV1 y del vocabulario
 text = "It's the last he painted, you know, Mrs. Gisburn said with pardonable pride."
 tokenizer = SimpleTokenizerV1(vocab)
 
-
-print("Codificando.")
+print("Codificando v1.")
 ids = tokenizer.encode(text)
 print("Texto: ", text)
 print("Codificado: ", ids)  
 print("Decodificado: ", tokenizer.decode(ids))
+print()
 
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+tokenizer = SimpleTokenizerV2(vocab)
+
+print("Codificando v2.")
+ids = tokenizer.encode(text)
+print("Texto: ", text)
+print("Codificado: ", ids)
+print("Decodificado: ", tokenizer.decode(ids))
 
 
 
