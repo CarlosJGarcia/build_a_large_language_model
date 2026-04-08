1ab259f41f34da25d78a18e757e73ca717969b04
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-08 21:06:35 +0200

    2-7

--- 02/2-6.py
+++ 02/2-6.py
@@ -45,10 +45,15 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+# dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+#first_batch = next(data_iter)
+#print(first_batch)
+
+inputs, targets = next(data_iter)
+print("Inputs:\n", inputs)
+print("Targets:\n", targets)
 
 
 
