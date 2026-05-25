bce0fd4d0cca4ffaabadfc01fef36ecb552f43a7
CarlosJGarcia <cgarciams@hotmail.com>
2026-05-25 12:06:50 +0200

    04

--- 04/04-03.py
+++ 04/04-03.py
@@ -4,6 +4,9 @@
 
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
+
+IMAGE_FILE = "activation_functions.png"
 
 # Defino una clase GELU a partir de (herencia) la clase Module de la librería Torch Neural Network (torch.nn)
 class GELU(nn.Module):
@@ -18,5 +21,20 @@ class GELU(nn.Module):
             (x + 0.044715 * torch.pow(x, 3))
         ))
     
-print("Todo bien!")
-print()
+gelu, relu = GELU(), nn.ReLU()
+
+x = torch.linspace(-3, 3, 100)     #1
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize=(8, 3))
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{label} activation function")
+    plt.xlabel("x")
+    plt.ylabel(f"{label}(x)")
+    plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+plt.savefig(IMAGE_FILE, dpi=300) 
+print(f"Plot saved successfully as {IMAGE_FILE}")
