a3fe24f0417931df4d2bfab7d1cccdd6ba1fc9c4
CarlosJGarcia <cgarciams@hotmail.com>
2026-05-25 12:28:20 +0200

    04

--- 04/04-03.py
+++ 04/04-03.py
@@ -6,7 +6,24 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+NUM_HEADS = 12
+CONTEXT_LENGHT = 1024
 IMAGE_FILE = "activation_functions.png"
+
+# Input and output embedding size (no se usan, pero las pongo por referencia)
+d_in = 768
+d_out = 768
+
+# Python dictionary. Configuration of the small GPT-2 model
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,               # Vocabulary size
+    "context_length": CONTEXT_LENGHT,  # Context length
+    "emb_dim": 768,                    # Embedding dimension
+    "n_heads": NUM_HEADS,              # Number of attention heads
+    "n_layers": 12,                    # Number of layers
+    "drop_rate": 0.1,                  # Dropout rate
+    "qkv_bias": False                  # Query-Key-Value bias
+}
 
 # Defino una clase GELU a partir de (herencia) la clase Module de la librería Torch Neural Network (torch.nn)
 class GELU(nn.Module):
@@ -21,6 +38,21 @@ class GELU(nn.Module):
             (x + 0.044715 * torch.pow(x, 3))
         ))
     
+
+# A feed forward neural network module
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+# Comparison between GELU() and nn.ReLU()
 gelu, relu = GELU(), nn.ReLU()
 
 x = torch.linspace(-3, 3, 100)     #1
@@ -38,3 +70,9 @@ plt.show()
 
 plt.savefig(IMAGE_FILE, dpi=300) 
 print(f"Plot saved successfully as {IMAGE_FILE}")
+
+# Initialize a FeedForward module with a token embedding size of 768 and feed it a batch input with two samples and three tokens each:
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)          
+out = ffn(x)
+print(out.shape)
---
+++ 04/04-04.py
@@ -0,0 +1,71 @@
+# A deep neural network with five layers, each consisting of a Linear layer and a GELU activation function.
+
+import torch
+import torch.nn as nn
+
+# Defino una clase GELU a partir de (herencia) la clase Module de la librería Torch Neural Network (torch.nn)
+class GELU(nn.Module):
+    # Contructor de la clase GELU que llama al constructor de la superclase (Module)
+    def __init__(self):
+        super().__init__()
+
+    # Override (sobreescritura de un método de la superclase, con un nuevo método)
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * 
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([       #1
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), 
+                          GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), 
+                          GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), 
+                          GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), 
+                          GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), 
+                          GELU())
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)         #2
+            if self.use_shortcut and x.shape == layer_output.shape:    #3
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
+
+# A function that computes the gradients in the model’s backward pass:
+def print_gradients(model, x):
+    output = model(x)             #1
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)    #2
+
+    loss.backward()          #3
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+layer_sizes = [3, 3, 3, 3, 3, 1]  
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)                            #1
+
+model_without_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=False
+)
+
+# Use the print_gradients function and apply it to the model without skip connections
+print_gradients(model_without_shortcut, sample_input)
+
+print()
