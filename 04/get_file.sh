7a8ff42236a6ed6c6dcd0e2b5f34ddb0fc5f2e29
CarlosJGarcia <cgarciams@hotmail.com>
2026-05-25 16:59:59 +0200

    File Copy

--- 04/get_file.sh
+++ 04/get_file.sh
@@ -1,4 +1,4 @@
 # Ejecutar este comando en el mac, para coger el fichero de la workstation
 # Hacer scp push de la workstation al mac es complicado porque el mac por defecto no acepta la petición scp
 
-scp garcic28@rbauw0006.kau.roche.com:~/Documents/python/build_a_large_language_model/04/activation_functions.png ~/Desktop/
+scp user@workstation.fqdn:~/Documents/python/build_a_large_language_model/04/activation_functions.png ~/Desktop/
