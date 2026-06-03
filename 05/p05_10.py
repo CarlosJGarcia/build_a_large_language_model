61736f3d5ab634f6b1f5c21f67b7c85060b14a2f
CarlosJGarcia <cgarciams@hotmail.com>
2026-06-03 20:47:19 +0200

    05-10

--- 05/p05_10.py
+++ 05/p05_10.py
@@ -1,6 +1,87 @@
+from p05_04 import GPTModel, generate_and_print_sample
 from p05_04 import GPT_CONFIG_124M
 
+# from gpt_download import download_and_load_gpt2
+
 import torch
+import numpy as np
+from rich.console import Console
+
+# Utility function that checks whether two tensors or arrays (left and right) have the same dimensions or shape and returns the right tensor as trainable PyTorch parameters
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, "
+                          "Right: {right.shape}"
+        )
+    return torch.nn.Parameter(torch.tensor(right))
+
+# Function that loads the weights from the params dictionary into a GPTModel instance gpt.
+def load_weights_into_gpt(gpt, params):                                                    #1
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+
+    for b in range(len(params["blocks"])):                                                 #2
+        q_w, k_w, v_w = np.split(                                                          #3
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(
+            gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(
+            gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(
+            gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight, 
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias, 
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight, 
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, 
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight, 
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias, 
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, 
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, 
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, 
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, 
+            params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])                  #4
+#1 Sets the model’s positional and token embedding weights to those specified in params.
+#2 Iterates over each transformer block in the model
+#3 The np.split function is used to divide the attention and bias weights into three equal parts for the query, key, and value components.
+#4 The original GPT-2 model by OpenAI reused the token embedding weights in the output layer to reduce the total number of parameters, which is a concept known as weight tying.
+
+
+
 
 # Dictionary that lists the differences between the different GPT model sizes
 model_configs = {
@@ -13,5 +94,27 @@ model_configs = {
 model_name = "gpt2-small (124M)"
 NEW_CONFIG = GPT_CONFIG_124M.copy()
 NEW_CONFIG.update(model_configs[model_name])
+
+NEW_CONFIG.update({"context_length": 1024})
+
+NEW_CONFIG.update({"qkv_bias": True})
+
+# Descarga
+settings, params = download_and_load_gpt2(model_size="124M", models_dir="gpt2")
+
+model = GPTModel(NEW_CONFIG)
+model.eval()
+
+# Set Up Processing Engine
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+console = Console()
+if torch.cuda.is_available():
+    console.print(f"Loading model on GPU: {torch.cuda.get_device_name(0)}", style="bright_blue", highlight=False)
+else:
+    console.print(f"CUDA not available. Loading model on CPU.", style="gold1") 
+
+load_weights_into_gpt(model, params)
+model.to(device)
+
 
 print("Todo bien")
