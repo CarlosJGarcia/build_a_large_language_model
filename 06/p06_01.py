f550decfc402d740b3168fe7aa3699921f15f319
CarlosJGarcia <cgarciams@hotmail.com>
2026-06-05 06:21:37 +0200

    06-01

---
+++ 06/p06_01.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+import urllib.request
+from pathlib import Path
+
+URL = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+ZIP_PATH = "sms_spam_collection.zip"
+EXTRACTED_PATH = "sms_spam_collection"
+data_file_path = Path(EXTRACTED_PATH) / "SMSSpamCollection.tsv"
+
+
+def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exists():
+        print(f"{data_file_path} already exists. Skipping download and extraction.")
+        return
+
+    # Downloads the file
+    with urllib.request.urlopen(url) as response:                                
+        with open(zip_path, "wb") as out_file:
+            out_file.write(response.read())
+
+    # Unzips the file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:                              
+        zip_ref.extractall(extracted_path)
+
+    original_file_path = Path(extracted_path) / "SMSSpamCollection"
+    # Adds .tsv file extension
+    os.rename(original_file_path, data_file_path)                                
+    print(f"File downloaded and saved as {data_file_path}")
+
+download_and_unzip_spam_data(URL, ZIP_PATH, EXTRACTED_PATH, data_file_path)
