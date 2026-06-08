6b0e9180e450b42ef8e2ed65c67308bdd86a1cd3
CarlosJGarcia <cgarciams@hotmail.com>
2026-06-08 09:38:30 +0200

    06-02

--- 06/p06_02.py
+++ 06/p06_02.py
@@ -1,6 +1,16 @@
-import pandas as pd
+# Procesa el fichero SMSSpamCollection.tsv
+# Genera un dataset equilibrado (50% spam, 50% ham) y dividido en proporciones 70/10/20 Training/Validation/Test
+# Guarda el resultado en tres ficheros .csv
+# Reinach 08/Jun/2026
 
+import pandas as pd
+from pathlib import Path
 from p06_01 import EXTRACTED_PATH, data_file_path
+
+WRITE_PATH = "../data/processed/sms_spam_collection"
+train_file_path = Path(WRITE_PATH) / "train.csv"
+validation_file_path = Path(WRITE_PATH) / "validation.csv"
+test_file_path = Path(WRITE_PATH) / "test.csv"
 
 def create_balanced_dataset(df):
     num_spam = df[df["Label"] == "spam"].shape[0]     #1
@@ -55,4 +65,12 @@ balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
 
 # Test size is implied to be 0.2 as the remainder.
 train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)                    
-print("Fin")
+
+# Save the dataset as CSV (comma-separated value) files so we can reuse it later:
+train_df.to_csv(train_file_path, index=None)
+print(f"File saved as {train_file_path}")
+validation_df.to_csv(validation_file_path, index=None)
+print(f"File saved as {validation_file_path}")
+test_df.to_csv(test_file_path, index=None)
+print(f"File saved as {test_file_path}")
+print()
