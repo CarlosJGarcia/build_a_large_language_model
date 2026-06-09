92dfbef31258101007db55767fd2750438725792
CarlosJGarcia <cgarciams@hotmail.com>
2026-06-09 06:39:03 +0200

    06-03

--- 06/p06_03.py
+++ 06/p06_03.py
@@ -109,7 +109,7 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
 
 
             with torch.no_grad():
-                logits = model(input_batch)[:, -1, :]              # Logits of last output token1
+                logits = model(input_batch)[:, -1, :]               # Logits of last output token1
             predicted_labels = torch.argmax(logits, dim=-1)
 
             num_examples += predicted_labels.shape[0]
@@ -120,6 +120,32 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
         else:
             break
     return correct_predictions / num_examples
+
+def calc_loss_batch(input_batch, target_batch, model, device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)[:, -1, :]                           # Logits of last output token
+    loss = torch.nn.functional.cross_entropy(logits, target_batch)
+    return loss
+
+# Calculating the classification loss
+def calc_loss_loader(data_loader, model, device, num_batches=None):
+    total_loss = 0.
+    if len(data_loader) == 0:
+        return float("nan")
+    elif num_batches is None:
+        num_batches = len(data_loader)
+    else:                                        #1
+        num_batches = min(num_batches, len(data_loader))
+    for i, (input_batch, target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            total_loss += loss.item()
+        else:
+            break
+    return total_loss / num_batches
 
 
 console = Console()
