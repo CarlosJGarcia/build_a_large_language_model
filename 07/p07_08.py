8a2867a01fb4cd160316b4319812d9e4ef0a0097
CarlosJGarcia <cgarciams@hotmail.com>
2026-06-12 20:52:22 +0200

    07-08

---
+++ 07/p07_08.py
@@ -0,0 +1,47 @@
+import json
+import urllib.request
+
+def query_model(
+    prompt, 
+    model="llama3", 
+    url="http://localhost:11434/api/chat"
+):
+    data = {             #1
+        "model": model,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ],
+        "options": {         #2
+            "seed": 123,
+            "temperature": 0,
+            "num_ctx": 2048
+        }
+    }
+
+    payload = json.dumps(data).encode("utf-8")    #3
+    request = urllib.request.Request(                       #4
+        url,                                                #4
+        data=payload,                                       #4
+        method="POST"                                       #4
+    ) #4
+
+    request.add_header("Content-Type", "application/json")   #4
+
+    response_data = ""
+    with urllib.request.urlopen(request) as response:   #5
+        while True:
+            line = response.readline().decode("utf-8")
+            if not line:
+                break
+            response_json = json.loads(line)
+            response_data += response_json["message"]["content"]
+
+    return response_data
+
+URL = "http://workstation.fqdn:11434/api/chat"
+
+# Main
+model = "llama3"
+print("Quering:", URL)
+result = query_model("What is a Radio Amateur?", model, url=URL)
+print(result)
